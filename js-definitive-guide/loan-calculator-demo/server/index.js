1aa8adfb55fdd47284ec651a00800582b5f8fea2
samuelchengx <676822037@qq.com>
2018-11-19 17:54:32 +0800

    feat: 还原权威指南第六版-js贷款计算器

---
+++ js-definitive-guide/loan-calculator-demo/server/index.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var app = express();
+
+var data = [
+    {url: 'https://baidu.com', name: '百度', id: 1},
+    {url: 'https://jd.com', name: '京东', id: 2}
+];
+
+// 处理get跨域问题
+var allowCrossDomain = function(req, res, next) {
+    // console.log('动态获取http源地址', req.headers.origin);
+    res.header('Access-Control-Allow-Origin', req.headers.origin);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    res.header('Access-Control-Allow-Credentials','true');
+    next();
+};
+
+app.use(allowCrossDomain);
+app.get('/getLenders', function (req,res) {
+    var zip = req.query.zip
+
+    console.log('zip', zip)
+    var resData = data.filter((item, index)=>{
+        // console.log('item, index', item, index)
+        return item.id == parseInt(zip);
+    });
+    if(zip != ''){
+        res.send(resData);
+    } else  {
+        res.send(data);
+    }
+
+});
+
+app.listen(8090);
+
